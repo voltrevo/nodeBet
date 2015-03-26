1f292f14f7a25d2da001d7fc0b3efb3e3fb0b51e
Andrew Morris <voltrevo@gmail.com>
2015-03-26 18:40:38 +1100

    Fixed alertify location and now using local install of browserify for build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+npm install
+mkdir -p log
 mkdir -p build
 cp -r lib/web build/.
-cp node_modules/alertifyjs/build/css/alertify.min.css node_modules/alertifyjs/build/css/themes/default.css build/web/css/. 
-browserify --debug lib/frontend/main.js -o build/web/frontend.js
+cp node_modules/alertifyjs/build/css/alertify.min.css node_modules/alertifyjs/build/css/themes/default.css build/web/css/alertify/.
+node node_modules/browserify/bin/cmd.js --debug lib/frontend/main.js -o build/web/frontend.js
