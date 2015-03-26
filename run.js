41acfb21ce0020f5503d623bcb7d1c7a4dad5c72
Andrew Morris <voltrevo@gmail.com>
2015-03-26 11:13:35 +1100

    Added forgotten run.js file from previous commit

---
+++ run.js
@@ -0,0 +1,24 @@
+"use strict"
+
+var fs = require("fs")
+
+var config = require("configure")
+var sockception = require("sockception")
+
+var exchange = require("./exchange")
+var log = require("./winstonWrapper")
+
+new exchange(
+    config.exchange,
+    {
+        read: function() {
+            return JSON.parse(fs.readFileSync(config.exchange.usersFile))
+        },
+        write: function(db) {
+            fs.writeFileSync(config.exchange.usersFile, JSON.stringify(db))
+        }
+    },
+    sockception.listen({
+        port: config.exchange.port,
+        log: log
+    }))
